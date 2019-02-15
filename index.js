2a2e3b64d784b40f3971fd12ca32a70607da4044
Andrewrowe1 <andrew_rowe1@yahoo.co.uk>
2019-02-15 14:09:33 +0000

    Pokemon recommit

--- index.js
+++ index.js
@@ -1,51 +1,81 @@
-function Pokemon(name, type='Normal') {
+class Pokemon {
+  constructor(name, type = "Normal") {
     this.pokemon = name;
     this.HP = 0;
-    this.atkDmg= 0;
-    this.sound= sound(type);
-    this.move= move(type);
-    this.type= type;
-    this.strongVS= strongVS(type);
-    this.weakVS= weakVS(type);
-    //Pokemon traits also includes all functions in pokemonActions
+    this.atkDmg = 0;
+    this.type = type;
+    this.sound = getSound(type);
+    this.move = getMove(type);
+
+    this.strongVS = getStrongVS(type);
+    this.weakVS = getWeakVS(type);
+  }
+  talk() {
+    return this.sound;
+  }
+  useYourMoves() {
+    return this.move;
+  }
 }
 
-const strongVS = function(type){
-    if (type === "Normal") return "None";
-    return type === "Fire" ? "Grass" : type === "Grass" ? "Water" : "Fire";
+function getStrongVS(type) {
+  return type === "Normal"
+    ? "None"
+    : type === "Fire"
+    ? "Grass"
+    : type === "Grass"
+    ? "Water"
+    : "Fire";
 }
 
-const weakVS = function(type){
-    if (type === "Normal") return "None";
-    return type === "Fire" ? "Water" : type === "Water" ? "Grass" : "Fire";
-
+function getWeakVS(type) {
+  return type === "Normal"
+    ? "None"
+    : type === "Fire"
+    ? "Water"
+    : type === "Water"
+    ? "Grass"
+    : "Fire";
+}
+function getSound(type) {
+  return type === "Normal"
+    ? "Squeak"
+    : type === "Fire"
+    ? "Crackle"
+    : type === "Grass"
+    ? "Grunt"
+    : "Gurgle";
+}
+function getMove(type) {
+  return type === "Normal"
+    ? "Tackle"
+    : type === "Fire"
+    ? "Fire Blast"
+    : type === "Grass"
+    ? "Razor Leaf"
+    : "Hydro Pump";
 }
 
-const sound = function(type){
-    if (type === "Normal") return "Squeak";
-    return type === "Fire" ? "Crackle" : type === "Grass" ? "Grunt" : "Gurgle";
-}
-
-const move = function(type){
-    if (type === "Normal") return "Tackle";
-    return type === "Fire" ? "Fire Blast" : type === "Grass" ? "Razor Leaf" : "Hydro Pump";
-}
-
-const pokemonActions = {
-    talk: function(){
-        return this.sound;
-    },
-    useYourMoves: function(){
-        return this.move;
-    }
-}
+//const pokemonActions = {};
 //  BULBASAUR = GRASS, CHARMANDER = FIRE, SQUIRTLE = WATER
 // RATATTA = NORMAL
-Pokemon.prototype = pokemonActions
+//Pokemon.prototype = pokemonActions;
 
+function Trainer(name) {
+  this.name = name;
+  this.storage = {};
+  this.quantity = 0;
+}
 
+const catchPokemon = {
+  catch: function(pokemon) {
+    quantity++;
+    this.pokemon[this.quantity] = new Pokemon(`${pokemon}`);
+  }
+};
 
+Trainer.prototype = catchPokemon;
 
+const Nick = new Trainer("Nick");
 
-
-module.exports = {Pokemon}
+module.exports = { Pokemon, Trainer };
