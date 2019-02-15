1e3b8e98c5f1499c91465ae9b9d84a7adc62b283
Nick-Dillon <ndillon18@hotmail.co.uk>
2019-02-15 15:52:41 +0000

    battle creation started

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 class Pokemon {
-  constructor(name, type = "Normal") {
+  constructor(name, type = "Normal", position = 0) {
     this.pokemon = name;
-    this.HP = 0;
-    this.atkDmg = 0;
+    this.position = position;
+    this.HP = 50;
+    this.atkDmg = 10;
     this.type = type;
     this.sound = getSound(type);
     this.move = getMove(type);
-
     this.strongVS = getStrongVS(type);
     this.weakVS = getWeakVS(type);
   }
@@ -16,6 +16,28 @@ class Pokemon {
   useYourMoves() {
     return this.move;
   }
+}
+
+class Trainer {
+    constructor(name) {
+        this.name = name;
+        this.pokemon = {};
+        this.quantity = 0;
+    }
+    catch(pokemon, type, position) {
+        this.quantity++;
+        this.pokemon[pokemon] = new Pokemon(`${pokemon}`, `${type}`, this.quantity);
+        return `${this.name} caught a ${pokemon}! They now have ${this.quantity} pokemon stored!`
+    }
+}
+
+class Battle {
+    constructor(trainerOne, pokemonOne, trainerTwo, pokemonTwo) {
+        this.trainerOne = trainerOne;
+        this.pokemonOne = pokemonOne;
+        this.trainerTwo = trainerTwo;
+        this.pokemonTwo = pokemonTwo;
+    }
 }
 
 function getStrongVS(type) {
@@ -56,26 +78,59 @@ function getMove(type) {
     : "Hydro Pump";
 }
 
-//const pokemonActions = {};
 //  BULBASAUR = GRASS, CHARMANDER = FIRE, SQUIRTLE = WATER
 // RATATTA = NORMAL
-//Pokemon.prototype = pokemonActions;
 
-function Trainer(name) {
-  this.name = name;
-  this.storage = {};
-  this.quantity = 0;
-}
-
-const catchPokemon = {
-  catch: function(pokemon) {
-    quantity++;
-    this.pokemon[this.quantity] = new Pokemon(`${pokemon}`);
-  }
-};
-
-Trainer.prototype = catchPokemon;
 
 const Nick = new Trainer("Nick");
 
-module.exports = { Pokemon, Trainer };
+module.exports = { Pokemon, Trainer, Battle };
+
+
+
+
+
+
+
+
+
+/*
+Finally, you will need a way to battle the pokemon.
+
+        create a battle class {
+            constructor(trainerOne, pokemonOne, trainerTwo, pokemonTwo) {
+                this.trainerOne = trainerOne;
+                this.pokemonOne = trainerOne.pokemon[pokemonOne];
+                this.trainerTwo = trainerTwo;
+                this.pokemonTwo = trainerTwo.pokemon[pokemonTwo];
+            }
+        }
+
+
+
+
+The battle should take two trainers and the names of the pokemon they wish to battle.
+    
+        make a function called haveFight(trainerOne, pokemonOne, trainerTwo, pokemonTwo)
+            let trainerFight = new Battle(trainerOne, pokemonOne, trainerTwo, pokemonTwo)
+
+
+
+
+The battle should have a fight method available.
+     This should take the pokemon whose turn it is, attack the defending pokemon (deducting attacker's attack damage from the defender's hit points), and end their turn.
+
+
+
+
+The fight method should take each pokemon's strengths and weaknesses into account. 
+
+If a defender is strong against the attacking type, the attacking type's damage should be multiplied by 0.75. 
+
+If a defender is weak against the attacking type, the attacking type's damage should be multiplied by 1.25.
+
+
+Each attack should be followed by an attack message. The message will vary depending on the defender's weakness/strength.
+
+
+If the defending pokemon faints (depletes all hit points), the attacker wins.
